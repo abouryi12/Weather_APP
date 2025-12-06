7b97c1cb7e5388cd7b4b0ed72ce0ea104048f66d
abouryi12 <abdallahabouryi@gmail.com>
2025-12-06 14:30:41 +0200

    Add full Flutter project files

--- lib/views/view_weather.dart
+++ lib/views/view_weather.dart
@@ -10,60 +10,70 @@ class ViewWeather extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          weatherModel.cityName,
-          style: TextStyle(
-            fontSize: Variables.heightScreen(context) * 0.05,
-            fontWeight: FontWeight.bold,
+    return Container(
+      decoration: BoxDecoration(
+        gradient:LinearGradient(colors:[Colors.blue,
+        Colors.pink
+        ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter
+        )
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            weatherModel.cityName,
+            style: TextStyle(
+              fontSize: Variables.heightScreen(context) * 0.05,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        Text("Updated at : ${weatherModel.data.hour}"),
-        SizedBox(height: Variables.heightScreen(context) * 0.03),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network("https:${weatherModel.icon}"),
-            SizedBox(width: Variables.widthScreen(context) * 0.15),
-            Text(
-              "${weatherModel.temp.round()}°C",
-              style: TextStyle(
-                fontSize: Variables.heightScreen(context) * 0.04,
+          Text("Updated at : ${weatherModel.data.hour}"),
+          SizedBox(height: Variables.heightScreen(context) * 0.03),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network("https:${weatherModel.icon}"),
+              SizedBox(width: Variables.widthScreen(context) * 0.15),
+              Text(
+                "${weatherModel.temp.round()}°C",
+                style: TextStyle(
+                  fontSize: Variables.heightScreen(context) * 0.04,
+                ),
               ),
-            ),
-            SizedBox(width: Variables.widthScreen(context) * 0.15),
-            Column(
-              children: [
-                Text(
-                  "Max: ${weatherModel.max.round()}",
-                  style: TextStyle(
-                    fontSize: Variables.heightScreen(context) * 0.017,
-                    fontWeight: FontWeight.bold,
+              SizedBox(width: Variables.widthScreen(context) * 0.15),
+              Column(
+                children: [
+                  Text(
+                    "Max: ${weatherModel.max.round()}",
+                    style: TextStyle(
+                      fontSize: Variables.heightScreen(context) * 0.017,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Text(
-                  "Min: ${weatherModel.min.round()}",
-                  style: TextStyle(
-                    fontSize: Variables.heightScreen(context) * 0.017,
-                    fontWeight: FontWeight.bold,
+                  Text(
+                    "Min: ${weatherModel.min.round()}",
+                    style: TextStyle(
+                      fontSize: Variables.heightScreen(context) * 0.017,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
-        SizedBox(height: 20),
-        Text(
-          weatherModel.weatherState!,
-          style: TextStyle(
-            fontSize: Variables.heightScreen(context) * 0.035,
-            fontWeight: FontWeight.bold,
+                ],
+              ),
+            ],
           ),
-        ),
-        SizedBox(height: Variables.heightScreen(context) * 0.15),
-      ],
+          SizedBox(height: 20),
+          Text(
+            weatherModel.weatherState!,
+            style: TextStyle(
+              fontSize: Variables.heightScreen(context) * 0.035,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: Variables.heightScreen(context) * 0.15),
+        ],
+      ),
     );
   }
 }
